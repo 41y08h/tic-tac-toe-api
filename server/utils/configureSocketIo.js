8144893d640073135a230a9a59b5dbb0ae57887c
Piyush <piyush41y08h@gmail.com>
2021-02-05 15:26:25 +0530

    Fix cors config --4

--- server/utils/configureSocketIo.js
+++ server/utils/configureSocketIo.js
@@ -1,7 +1,10 @@
 const config = require("../config");
 
 function configureSocketIo(server) {
-  const io = require("socket.io")(server, { cors: { origin: "*" } });
+  const io = require("socket.io")(server, {
+    credentials: true,
+    origin: (origin, callback) => callback(null, true),
+  });
 
   return io;
 }
